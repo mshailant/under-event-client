33164d3cd577f049f1f088287f4e342522acabb3
sebitacasa <seba.-14@hotmail.com>
2022-04-28 11:14:43 -0300

    componentes Nav creados con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import  Home  from './components/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+
+
+
+
 
 
 export default function Individual({id, title, imagen,}){
   return(
-      <div >
-        <div key={id}></div>          
-        <h2>{title}</h2>
-        <img src={imagen} alt="no hay imagen" width="200px" heigth="250"/>
+    <div className="grid" key={id}>
 
-      </div>
+    <Card style={{ width: '18rem' }} className="box">
+    <Card.Img variant="top" src={imagen} />
+    <Card.Body>
+      <Card.Title>{title}</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button class="btn btn-outline-primary" variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  </div>
+  
   )
 }
