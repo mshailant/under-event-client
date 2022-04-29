859a57f4a6ea641b7e69c01917a881180f162578
msh <mshailant@gmail.com>
2022-04-29 14:42:51 -0300

    se agrego ruta de perfil de dusuario

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import CreateEvent from "./components/CreateEvent";
 import Nav from "./components/Nav";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Nav />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/profile" element={<UserProfile />} />
         <Route exact path="/createEvent" element={<CreateEvent />} />
       </Routes>
     </BrowserRouter>
