21136da749987a84f453fe669621dae4e162aca2
msh <mshailant@gmail.com>
2022-05-04 03:56:13 -0300

    se agrego action para obtener un usuario

--- src/redux/actions/actions.jsx
+++ src/redux/actions/actions.jsx
@@ -6,8 +6,9 @@ export const GET_BY_TITLE = "GET_BY_TITLE";
 export const GET_DETAIL = "GET_DETAIL";
 
 export const BY_EVENT_TYPE = "BY_EVENT_TYPE";
-export const GET_STATES = "GET_STATES"
+export const GET_STATES = "GET_STATES";
 
+export const GET_USER = "GET_USER";
 
 export function getAllEvent() {
   return async function (dispatch) {
@@ -42,7 +43,9 @@ export function createEvent(payload) {
 
 export function getByTitle(title) {
   return async (dispatch) => {
-    let obtener = await axios.get(`http://localhost:3001/events/getTitle?title=${title}`);
+    let obtener = await axios.get(
+      `http://localhost:3001/events/getTitle?title=${title}`
+    );
     return dispatch({
       type: GET_BY_TITLE,
       payload: obtener.data,
@@ -52,7 +55,7 @@ export function getByTitle(title) {
 export function getDetail(id) {
   return async (dispatch) => {
     let json = await axios.get("http://localhost:3001/events/" + id);
-    
+
     return dispatch({
       type: GET_DETAIL,
       payload: json.data,
@@ -63,17 +66,26 @@ export function getDetail(id) {
 //filtar por tipo de evento
 export function byEventType(payload) {
   return {
-      type: BY_EVENT_TYPE,
-      payload
-  }
+    type: BY_EVENT_TYPE,
+    payload,
+  };
 }
-
-
 
 export function getState(payload) {
   return {
-      type: GET_STATES,
-      payload
-  }
+    type: GET_STATES,
+    payload,
+  };
 }
 
+export function getUserByEmail(email) {
+  return async (dispatch) => {
+    let json = await axios.get(
+      `http://localhost:3001/users/getUser?email=${email}`
+    );
+    return dispatch({
+      type: GET_USER,
+      payload: json.data,
+    });
+  };
+}
--- src/redux/reducer/reducer.jsx
+++ src/redux/reducer/reducer.jsx
@@ -4,14 +4,17 @@ const InitialState = {
 
   eventosBack: [],
 
-  detailEventos: {},	
+  detailEventos: {},
 
   allEventType: [],
 
   filterTime: [],
 
-  allEventState: []
+  allEventState: [],
 
+  userLogedin: {},
+
+  users: [],
 };
 
 function rootReducer(state = InitialState, action) {
@@ -22,62 +25,54 @@ function rootReducer(state = InitialState, action) {
         eventosBack: action.payload,
         eventosDb: action.payload,
         allEventType: action.payload,
-        allEventState: action.payload
+        allEventState: action.payload,
       };
     case Action.CREATE_EVENT:
       return {
         ...state,
       };
-    
+
     case Action.GET_DETAIL:
-      
       return {
         ...state,
         detailEventos: action.payload,
       };
     case Action.GET_BY_TITLE:
-      return{
+      return {
         ...state,
         eventosDb: action.payload,
-        
-      }
+      };
 
-   
-      case Action.GET_STATES:
-        const allState = state.allEventState;
-        const stateFilter =
-          action.payload === "All"
-            ? allState
-            : allState.filter((g) =>
-                g.state === action.payload
-              );
-        return {
-          ...state,
-          
-          eventosDb: stateFilter
-          
-        
+    case Action.GET_STATES:
+      const allState = state.allEventState;
+      const stateFilter =
+        action.payload === "All"
+          ? allState
+          : allState.filter((g) => g.state === action.payload);
+      return {
+        ...state,
 
-        };
+        eventosDb: stateFilter,
+      };
 
-        case Action.BY_EVENT_TYPE:
-        const allType = state.allEventType;
-        const typeFilter =
-          action.payload === "All"
-            ? allType
-            : allType.filter((g) =>
-                g.eventType === action.payload
-              );
-        return {
-          ...state,
-         
-          eventosBack: typeFilter,
-          eventosDb: typeFilter
-          
-        
+    case Action.BY_EVENT_TYPE:
+      const allType = state.allEventType;
+      const typeFilter =
+        action.payload === "All"
+          ? allType
+          : allType.filter((g) => g.eventType === action.payload);
+      return {
+        ...state,
 
-        };
-      
+        eventosBack: typeFilter,
+        eventosDb: typeFilter,
+      };
+
+    case Action.GET_USER:
+      return {
+        ...state,
+        userLogedin: action.payload,
+      };
 
     default:
       return {
