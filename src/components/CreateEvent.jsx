f5d6cc50b18c76b9edbb6d7a8608f6afd80df666
msh <mshailant@gmail.com>
2022-05-04 04:00:05 -0300

    ahora se debe estar logueado para crear un evento

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -2,19 +2,19 @@ import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { createEvent } from "../redux/actions/actions";
-import styles from  "./CreateEvent.module.css";
-import { Button, FormControl, Col, Row, Container} from "react-bootstrap";
-import Footer from './Footer/Footer';
-import {Nav, Navbar, NavDropdown} from "react-bootstrap"
-import {LinkContainer} from 'react-router-bootstrap'
+import styles from "./CreateEvent.module.css";
+import { Button, FormControl, Col, Row, Container } from "react-bootstrap";
+import Footer from "./Footer/Footer";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 export function validate(input) {
   let errors = {};
 
   if (!input.title) {
     errors.title = "title is require";
-  }
-  else if (!input.description) {
+  } else if (!input.description) {
     errors.description = "Please description is require";
   }
   // if (!input.performers.length <= 0) {
@@ -22,30 +22,26 @@ export function validate(input) {
   // }
   else if (!input.imagen) {
     errors.imagen = "Please image is require";
-  }
-  else if (!input.date) {
+  } else if (!input.date) {
     errors.date = "Please enter the date";
-  }
-  else if (!input.time) {
+  } else if (!input.time) {
     errors.time = "Please insert a valid time";
-  }
-  else if (!input.stock) {
+  } else if (!input.stock) {
     errors.stock = "Please, insert how many tickets you want to buy";
-  }
-  else if (!input.eventType) {
+  } else if (!input.eventType) {
     errors.eventType = "Please, insert type of event ";
-  }
-  else if (!input.state) {
+  } else if (!input.state) {
     errors.state = "Please, insert state you want to buy";
-  }
-  else if (!input.place) {
+  } else if (!input.place) {
     errors.state = "Please, insert place";
   }
 
   return errors;
 }
 
-export default function CreateEvent() {
+export function CreateEvent() {
+  const { user, isLoading } = useAuth0();
+
   const stateInitialForms = {
     title: "",
     description: "",
@@ -56,7 +52,7 @@ export default function CreateEvent() {
     time: "",
     stock: "",
     state: "",
-    place: ""
+    place: "",
   };
 
   const [input, setInput] = useState(stateInitialForms);
@@ -71,12 +67,11 @@ export default function CreateEvent() {
       input.description === "" &&
       // input.performers === "",
       input.eventType === "" &&
-     
       input.imagen === "" &&
       input.date === "" &&
       input.time === "" &&
-      input.stock === ""&&
-      input.state === ""&&
+      input.stock === "" &&
+      input.state === "" &&
       input.place === ""
     ) {
       alert("Please complete the form");
@@ -93,7 +88,7 @@ export default function CreateEvent() {
         stock: "",
         eventType: "",
         state: "",
-        place: ""
+        place: "",
       });
     }
   };
@@ -130,90 +125,81 @@ export default function CreateEvent() {
 
   return (
     <div>
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-      <LinkContainer to = "/">
-  <Navbar.Brand style={{}} >UnderEventApp</Navbar.Brand>
-  </LinkContainer>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-    
-    </Nav>
-    <Nav>
-    
-   
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-    <div className={styles.containerCreate}>
-      <div className={styles.containerInput}>
-        
-         
-            
-            <div className={styles.container}>
-          <div className={styles.img}>
-            {/* <img 
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand style={{}}>UnderEventApp</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav></Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className={styles.containerCreate}>
+        <div className={styles.containerInput}>
+          <div className={styles.container}>
+            <div className={styles.img}>
+              {/* <img 
             style={{borderRadius: 8, }}
             className="d-block w-50"
             src={Event}
             alt="First slide"
           />  */}
-          </div>
-        
-        <div className={styles.container}>
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <div className={styles.title}>
-              <h2>Create your Events</h2>
             </div>
 
-            <div className={styles.allLabel}>
-              <label className={styles.labelInput} >Title:</label>
+            <div className={styles.container}>
+              <form onSubmit={(e) => handleSubmit(e)}>
+                <div className={styles.title}>
+                  <h2>Create your Events</h2>
+                </div>
 
-              <input
-                type="text"
-                name="title"
-                value={input.title}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-              />
-              <div className={styles.alert}>
-              {errors.title && <p className={styles.danger}>{errors.title}</p>}
-              </div>
+                <div className={styles.allLabel}>
+                  <label className={styles.labelInput}>Title:</label>
 
-              <label className={styles.labelInput} >Description:</label>
-              <input
-                type="text"
-                name="description"
-                value={input.description}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-              />
-              <div className={styles.alert} >
-              {errors.description && (
-                <p className={styles.danger}>{errors.description}</p>
-              )}
-              </div>
+                  <input
+                    type="text"
+                    name="title"
+                    value={input.title}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.title && (
+                      <p className={styles.danger}>{errors.title}</p>
+                    )}
+                  </div>
 
-              <label className={styles.labelInput} >Place:</label>
-              <input
-                type="text"
-                name="place"
-                value={input.place}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.place}
-              />
-              <div className={styles.alert} >
-              {errors.place && (
-                <p className={styles.danger}>{errors.place}</p>
-              )}
-              </div>
+                  <label className={styles.labelInput}>Description:</label>
+                  <input
+                    type="text"
+                    name="description"
+                    value={input.description}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.description && (
+                      <p className={styles.danger}>{errors.description}</p>
+                    )}
+                  </div>
 
-             
+                  <label className={styles.labelInput}>Place:</label>
+                  <input
+                    type="text"
+                    name="place"
+                    value={input.place}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.place}
+                  />
+                  <div className={styles.alert}>
+                    {errors.place && (
+                      <p className={styles.danger}>{errors.place}</p>
+                    )}
+                  </div>
 
-             
-              {/* <label>Performers:</label>
+                  {/* <label>Performers:</label>
            <input
             type="text"
             name="performers"
@@ -221,97 +207,100 @@ export default function CreateEvent() {
             onChange={(e) => handlePerformers(e)}
           />
           {errors.performers && <p className="danger">{errors.performers}</p>}  */}
-              <label className={styles.labelInput}>Imagen:</label>
-              <input
-                type="text"
-                name="imagen"
-                value={input.imagen}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-              />
-              <div className={styles.alert}>
-              {errors.imagen && <p className={styles.danger}>{errors.imagen}</p>}
-              </div>
-              <label className={styles.labelInput}>Date:</label>
-              <input
-                type="text"
-                name="date"
-                value={input.date}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-              />
-              <div className={styles.alert} >
-              {errors.date && <p className={styles.danger}>{errors.date}</p>}
-              </div>
-              <label className={styles.labelInput} >Time:</label>
-              <input
-                type="text"
-                name="time"
-                value={input.time}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-                
-              />
-              <div className={styles.alert} >
-              {errors.time && <p className={styles.danger}>{errors.time}</p>}
-              </div>
-              <label className={styles.labelInput} >Tickets:</label>
-              <input
-                type="text"
-                name="stock"
-                value={input.stock}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-              />
-              <div className={styles.alert}>
-              {errors.stock && <p className={styles.danger}>{errors.stock}</p>}
-              </div>
-              <label className={styles.labelInput} >Event Type:</label>
-              <input
-                type="text"
-                name="eventType"
-                value={input.eventType}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-              />
-              <div className={styles.alert}>
-              {errors.stock && <p className={styles.danger}>{errors.stock}</p>}
-              </div>
-              <label className={styles.labelInput}>State:</label>
-              <input
-                type="text"
-                name="state"
-                value={input.state}
-                onChange={(e) => handleInputChange(e)}
-                className={styles.allInput}
-              />
-              <div className={styles.alert}>
-              {errors.state && <p className={styles.danger}>{errors.state}</p>}
-              </div>
+                  <label className={styles.labelInput}>Imagen:</label>
+                  <input
+                    type="text"
+                    name="imagen"
+                    value={input.imagen}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.imagen && (
+                      <p className={styles.danger}>{errors.imagen}</p>
+                    )}
+                  </div>
+                  <label className={styles.labelInput}>Date:</label>
+                  <input
+                    type="text"
+                    name="date"
+                    value={input.date}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.date && (
+                      <p className={styles.danger}>{errors.date}</p>
+                    )}
+                  </div>
+                  <label className={styles.labelInput}>Time:</label>
+                  <input
+                    type="text"
+                    name="time"
+                    value={input.time}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.time && (
+                      <p className={styles.danger}>{errors.time}</p>
+                    )}
+                  </div>
+                  <label className={styles.labelInput}>Tickets:</label>
+                  <input
+                    type="text"
+                    name="stock"
+                    value={input.stock}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.stock && (
+                      <p className={styles.danger}>{errors.stock}</p>
+                    )}
+                  </div>
+                  <label className={styles.labelInput}>Event Type:</label>
+                  <input
+                    type="text"
+                    name="eventType"
+                    value={input.eventType}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.stock && (
+                      <p className={styles.danger}>{errors.stock}</p>
+                    )}
+                  </div>
+                  <label className={styles.labelInput}>State:</label>
+                  <input
+                    type="text"
+                    name="state"
+                    value={input.state}
+                    onChange={(e) => handleInputChange(e)}
+                    className={styles.allInput}
+                  />
+                  <div className={styles.alert}>
+                    {errors.state && (
+                      <p className={styles.danger}>{errors.state}</p>
+                    )}
+                  </div>
+                </div>
+                <Button
+                  style={{ marginTop: 10, marginLeft: 140, marginTop: 45 }}
+                  variant="outline-light"
+                  type="submit"
+                >
+                  Create Event
+                </Button>
+              </form>
             </div>
-            <Button
-              style={{ marginTop: 10, marginLeft: 140, marginTop: 45 }}
-              variant="outline-light"
-              type="submit"
-            >
-              Create Event
-            </Button>
-          </form>
           </div>
         </div>
-        
-            
-            
-           
-           
-            
-          
-        
-        
       </div>
-      
-    </div>
-    <Footer/>
+      <Footer />
     </div>
   );
 }
+
+export default withAuthenticationRequired(CreateEvent);
