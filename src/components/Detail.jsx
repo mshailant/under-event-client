e581af6aed69565236679163a263600f99544335
msh <mshailant@gmail.com>
2022-05-04 04:00:55 -0300

    se quito la navbar y se agrego a app.js

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -5,23 +5,30 @@ import { getDetail } from "../redux/actions/actions.jsx";
 import { useParams } from "react-router";
 import styles from "./Detail.module.css";
 
-import { Card, Button, } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
-import {LinkContainer} from 'react-router-bootstrap'
+import { LinkContainer } from "react-router-bootstrap";
 
-import {Nav, Navbar, NavDropdown, Container, Row, Col, Figure, Form, Placeholder } from "react-bootstrap"
-import NavTop from "./NavBars/Nav.jsx";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Container,
+  Row,
+  Col,
+  Figure,
+  Form,
+  Placeholder,
+} from "react-bootstrap";
 
-
-import imagen from "../images/concert2.jpg"
+import imagen from "../images/concert2.jpg";
 import FooterPage from "./NewFooter.jsx";
-
 
 const Detail = () => {
   const dispatch = useDispatch();
   const detalles = useSelector((state) => state.detailEventos);
   const { id } = useParams();
-  console.log(detalles, "detallaso")
+  console.log(detalles, "detallaso");
 
   useEffect(() => {
     dispatch(getDetail(id));
@@ -31,119 +38,132 @@ const Detail = () => {
     window.location.href = "/";
   };
   return (
-    <div  >
+    <div>
+      {detalles ? (
+        <div className={styles.container}>
+          <Container style={{ background: "#ecf0f1", marginTop: "25px" }}>
+            <Row>
+              <Col xl>
+                <Figure>
+                  <Figure.Image
+                    width={500}
+                    height={500}
+                    alt="171x180"
+                    src={detalles.imagen}
+                  />
+                </Figure>
+              </Col>
+              <Col xs={{ order: 12 }}>
+                <div
+                  className={styles.container2}
+                  style={{ backGround: "black", height: "auto" }}
+                >
+                  <div className={styles.ticketContainer}>
+                    <Container>
+                      <Row>
+                        <Col xs>Tipo de Ticket</Col>
+                        <Col xs={{ order: 12 }}>Valor</Col>
+                        <Col xs={{ order: 1 }}>Cantidad</Col>
+                      </Row>
+                    </Container>
+                  </div>
+                  <div className={styles.ticketContainer}>
+                    <Container>
+                      <Row>
+                        <Col xs>Cuota {detalles.title}</Col>
+                        <Col xs={{ order: 12 }}>$ 3500 </Col>
+                        <Col xs={{ order: 1 }}>
+                          <Form.Select aria-label="Default select example">
+                            <option>0</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </Form.Select>
+                        </Col>
+                      </Row>
+                    </Container>{" "}
+                  </div>
+                  <div className={styles.ticketContainer}>
+                    <Container>
+                      <Row>
+                        <Col xs>First, but unordered</Col>
+                        <Col xs={{ order: 12 }}>Second, but last</Col>
+                        <Col xs={{ order: 1 }}>
+                          <Form.Select aria-label="Default select example">
+                            <option>0</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </Form.Select>
+                        </Col>
+                      </Row>
+                    </Container>{" "}
+                  </div>
 
-    {detalles ? (
-    <div className={styles.container} >
-    <NavTop/>
-  <Container style={{background: "#ecf0f1", marginTop: "25px"}}>
+                  <div className={styles.ticketContainer}>
+                    <Container>
+                      <Row>
+                        <Col xs>First, but unordered</Col>
+                        <Col xs={{ order: 12 }}>Second, but last</Col>
+                        <Col xs={{ order: 1 }}>
+                          <Form.Select aria-label="Default select example">
+                            <option>0</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </Form.Select>
+                        </Col>
+                      </Row>
+                    </Container>{" "}
+                  </div>
 
-  <Row>
-    <Col xl><Figure>
-<Figure.Image
-  width={500}
-  height={500}
-  alt="171x180"
-  src={detalles.imagen}
-/>
-
-</Figure></Col>
-    <Col  xs={{ order: 12 }}><div className={styles.container2} style={{backGround: "black", height: "auto" }}>
-     
-      <div className={styles.ticketContainer}><Container>
-  <Row >
-    <Col xs>Tipo de Ticket</Col>
-    <Col xs={{ order: 12 }}>Valor</Col>
-    <Col xs={{ order: 1 }}>Cantidad</Col>
-  </Row>
-</Container></div>
-      <div className={styles.ticketContainer} ><Container>
-  <Row >
-    <Col xs>Cuota {detalles.title}</Col>
-    <Col xs={{ order: 12 }}>$ 3500 </Col>
-    <Col xs={{ order: 1 }}><Form.Select aria-label="Default select example">
-  <option>0</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</Form.Select></Col>
-  </Row>
-</Container> </div>
-      <div className={styles.ticketContainer} ><Container>
-  <Row>
-    <Col xs>First, but unordered</Col>
-    <Col xs={{ order: 12 }}>Second, but last</Col>
-    <Col xs={{ order: 1 }}><Form.Select aria-label="Default select example">
-  <option>0</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</Form.Select></Col>
-  </Row>
-</Container> </div>
-
-<div className={styles.ticketContainer} ><Container>
-  <Row>
-    <Col xs>First, but unordered</Col>
-    <Col xs={{ order: 12 }}>Second, but last</Col>
-    <Col xs={{ order: 1 }}><Form.Select aria-label="Default select example">
-  <option>0</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</Form.Select></Col>
-  </Row>
-</Container> </div>
-
-<div className={styles.ticketContainer} ><Container>
-  <Row>
-    <Col xs>First, but unordered</Col>
-    <Col xs={{ order: 12 }}>Second, but last</Col>
-    <Col xs={{ order: 1 }}>Third, but second</Col>
-  </Row>
-</Container>
- </div>
- <Figure.Caption>
-  <div>
-    <div  > <hr/>
+                  <div className={styles.ticketContainer}>
+                    <Container>
+                      <Row>
+                        <Col xs>First, but unordered</Col>
+                        <Col xs={{ order: 12 }}>Second, but last</Col>
+                        <Col xs={{ order: 1 }}>Third, but second</Col>
+                      </Row>
+                    </Container>
+                  </div>
+                  <Figure.Caption>
+                    <div>
+                      <div>
+                        {" "}
+                        <hr />
+                      </div>
+                      <div className={styles.caption}>
+                        <>
+                          <p aria-hidden="false">
+                            <h2>{detalles.title}</h2>
+                          </p>
+                          <p aria-hidden="false">
+                            <p>{detalles.description}</p>
+                          </p>{" "}
+                          <hr />
+                          <div className={styles.containerDate}>
+                            <p aria-hidden="false">
+                              <h4>Fecha : {detalles.date}</h4>
+                            </p>
+                            <p aria-hidden="false">
+                              <h5>Horario : {detalles.time}</h5>
+                            </p>
+                          </div>
+                        </>
+                      </div>
+                    </div>
+                  </Figure.Caption>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <FooterPage />
+        </div>
+      ) : (
+        <h1>error</h1>
+      )}
     </div>
-    <div className={styles.caption} ><>
-  <p  aria-hidden="false">
-    <h2>{detalles.title}</h2>
-  </p>
-  <p aria-hidden="false">
-    <p>{detalles.description}</p>
-  </p> <hr/>
-  <div className={styles.containerDate}>
-  <p aria-hidden="false">
-    <h4>Fecha : {detalles.date}</h4>
-  </p>
-  <p aria-hidden="false">
-    <h5>Horario : {detalles.time}</h5>
-  </p>
-  </div>
-
-  
-</></div>
-
-
-  </div>
-</Figure.Caption>
-      </div>
-      
-      </Col>
-    
-  </Row>
-</Container>
-<FooterPage/>
-</div>): <h1>error</h1>}
-</div>
-
-
-    
   );
 };
 
 export default Detail;
-
-
