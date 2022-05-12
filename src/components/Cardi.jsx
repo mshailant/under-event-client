fdfa03b84b78dffed34219405528ea2c4664f760
sebitacasa <seba.-14@hotmail.com>
2022-05-12 18:30:26 -0300

    cambios en detalles y mapa

--- src/components/Cardi.jsx
+++ src/components/Cardi.jsx
@@ -13,19 +13,26 @@ import { LinkContainer } from "react-router-bootstrap";
 import style from "./Card.module.css";
 
 import { FaCalendar, FaSearchLocation, FaTicketAlt } from "react-icons/fa";
-import img from "../images/pexels-darya-sannikova-3824763.jpg"
+import img from "../images/pexels-darya-sannikova-3824763.jpg";
+import Foo from "./StartsComponent";
+import ReactStars from "react-rating-stars-component";
+
+import { render } from "react-dom";
 
 export default function Cardi({ id, title, imagen, description, date, place }) {
- 
+  const RatingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
   return (
     <Container>
       <Row>
         <Card
           className={style.cards}
           style={{
-            width: "18rem",
-            height: "520px",
-            width: "400px",
+            
+            width: "700px",
+            height: "600px",
             background: "#979a9a",
           }}
         >
@@ -34,21 +41,28 @@ export default function Cardi({ id, title, imagen, description, date, place }) {
             variant="top"
             src={imagen ? imagen : img}
           />
-          <Card.Body style={{ width: "400px" }}>
+          <Card.Body style={{ width: "400px", height: "auto"}}>
             <Card.Title>{title}</Card.Title>
           </Card.Body>
           <ListGroup
             style={{ background: "  #979a9a " }}
             className="list-group-flush"
           >
-            <ListGroupItem
-              style={{ background: "  #979a9a " }}
-            ></ListGroupItem>
+            <ListGroupItem style={{ background: "  #979a9a " }}></ListGroupItem>
             <ListGroupItem style={{ background: "  #979a9a " }}>
               <FaCalendar /> {date}
             </ListGroupItem>
             <ListGroupItem style={{ background: "  #979a9a  " }}>
               <FaSearchLocation /> {place}
+            </ListGroupItem>
+            <ListGroupItem style={{ background: "  #979a9a  ", height: "100px" }}>
+              <ReactStars
+                count={5}
+                onChange={RatingChanged}
+                size={30}
+                activeColor="#ffd700"
+              />
+              ,
             </ListGroupItem>
           </ListGroup>
           <Card.Body></Card.Body>
@@ -58,7 +72,7 @@ export default function Cardi({ id, title, imagen, description, date, place }) {
               style={{ width: "auto" }}
               variant="warning"
             >
-              <FaTicketAlt /> Add to cart
+              <FaTicketAlt /> Ver detalle del evento
             </Button>
           </LinkContainer>
         </Card>
