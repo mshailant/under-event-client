ef932730edc0a6ff8cfdabfee3a9c99da1303165
msh <mshailant@gmail.com>
2022-05-13 11:39:58 -0300

    se agrego ruta de carrito

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
 import { useSelector } from "react-redux";
 
 import history from "./utils/history";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import Loading from "./components/Loading";
-
 import CreateEvent from "./components/CreateEvent";
 
 import UserProfile from "./components/UserProfile";
 import UserManagement from "./components/UserManagement";
 import Detail from "./components/Detail";
-
-import NavTop from "./components/NavBars/Nav";
 import Pay from "./components/Payment";
 import Carrito from "./components/Carrito";
 import OrderDetail from "./components/OrderDetail";
+import Cart from "./components/Cart";
 
 function App() {
-  const { isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <Loading />;
-  }
+  const user = useSelector((state) => state.userLoged);
 
   return (
     <BrowserRouter history={history}>
@@ -33,7 +24,20 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route exact path="/profile" element={<UserProfile />} />
         <Route exact path="/createEvent" element={<CreateEvent />} />
-        <Route exact path="/userManagement" element={<UserManagement />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route
+          exact
+          path="/userManagement"
+          element={
+            user?.roll === "Admin" ? (
+              <UserManagement />
+            ) : (
+              <div>
+                <h1>No tienes permisos para acceder a esta seccion</h1>
+              </div>
+            )
+          }
+        />
         <Route exact path="/:id" element={<Detail />} />
         <Route exact path="/payment" element={<Pay />} />
         <Route exact path="/carrito" element={<Carrito />} />
