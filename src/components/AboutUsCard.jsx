2932017bb6c0a597a5fa1a5971334f83a44027cd
TomasTaglia <tomitaglia2000@gmail.com>
2022-05-17 17:01:26 -0300

    revisionComplete

--- src/components/AboutUsCard.jsx
+++ src/components/AboutUsCard.jsx
@@ -1,38 +1,34 @@
 import React from "react";
-import { Col, Row, Card, Container, CardGroup } from "react-bootstrap"
-import { BsGithub } from "react-icons/bs"
+import { Card } from "react-bootstrap"
+import { BsGithub, BsLinkedin } from "react-icons/bs"
+import styles from "./AboutUs.module.css"
 
-
-export default function AboutUsCard({ name, birthDay, description, image, github }){
+export default function AboutUsCard({ name, birthDay, description, image, github, linkedIn }){
+    console.log(linkedIn)
     return(
         <div>
-            <Row xs={1} md={5} className="g-4">
-            {Array.from({ length: 1 }).map((_, idx) => (
-                <Container>
-                    <Row>
-                    <Col>
-                        <Card >
-                            <Card.Img variant="top" src={image} />
-                            <Card.Body>
-                            <Card.Title>{name}</Card.Title>
-                            <Card.Text>
-                                {description}
-                            </Card.Text>
-                            <Card.Text>
-                                {birthDay}
-                            </Card.Text>
-                            <Card.Text>
-                            <a href={"https://github.com/" + github}>
-                                <BsGithub/>
-                            </a>
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            ))}
-            </Row>
+            <Card bg="dark" text="light" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg" />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+                <Card.Text>
+                    {birthDay}
+                </Card.Text>
+            </Card.Body>
+            <Card.Body>
+            <div className={styles.icon}>
+                <a href={"https://github.com/" + github}>
+                    <BsGithub />
+                </a>
+                <a href={"https://www.linkedin.com/in/" + linkedIn} className={styles.icon}>
+                    <BsLinkedin />
+                </a>
+            </div>
+            </Card.Body>
+            </Card>
         </div>
     )
 }
