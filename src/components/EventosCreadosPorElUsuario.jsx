cc79a82e38dd0b6bfd28be06e835f974718fdc9c
Pirez10 <francopirez95@gmail.com>
2022-05-18 00:27:53 -0300

    arregle una cosilla del perfil y ademas tengo novedades en detalle de ordenes

--- src/components/EventosCreadosPorElUsuario.jsx
+++ src/components/EventosCreadosPorElUsuario.jsx
@@ -36,30 +36,33 @@ export function EventosCreadosPorElUsuario() {
 
     // tomare solo el mail de los usuarios -----------> por que es lo unico que no se puede repetir entre usuarios
     // y con ello podre descubrir el id del usuario logueado
-    let soloEmails = allUser.map(e => e.email)
+    let soloEmails = allUser?.map(e => e.email)
     console.log(soloEmails, "soy solo los mails")
 
-    // ahora puedo obtener el id del usuario logueado
-    let idDelUsuarioLogueado;
-    for (let i = 0; i < soloEmails.length; i++){
-        if (soloEmails[i] == usuario.email){
-            /* var coincide = true
-            console.log(coincide) */
-            idDelUsuarioLogueado = allUser.map(e => e.id)
-        }  
-    }
-
-
-    console.log(idDelUsuarioLogueado, "soy el id del usuario logueado")
+    //aqui me traigo los eventos
+    const objetos = useSelector((state) => state.eventosBack);
+    /* console.log(objetos, "soy los eventos") */
 
 
 
+    try {
 
-    try{
 
-        //aqui me traigo los eventos
-        const objetos = useSelector((state) => state.eventosBack);
-        /* console.log(objetos, "soy los eventos") */
+        // ahora puedo obtener el id del usuario logueado
+        let idDelUsuarioLogueado;
+        for (let i = 0; i < soloEmails.length; i++) {
+            if (soloEmails[i] == usuario.email) {
+                /* var coincide = true
+                console.log(coincide) */
+                idDelUsuarioLogueado = allUser.map(e => e.id)
+            }
+        }
+
+
+        console.log(idDelUsuarioLogueado, "soy el id del usuario logueado")
+
+
+
 
         //quiero recorrerlos
 
@@ -70,74 +73,74 @@ export function EventosCreadosPorElUsuario() {
         let hora = []
         let precio = []
 
-        for (let i=0; i < objetos.length; i++){
-             if(objetos[i].UserId == idDelUsuarioLogueado/* MyUserId */){
-                 nombre.push(objetos[i].title)
-                 stock.push(objetos[i].stock)
-                 ciudad.push(objetos[i].city)
-                 fecha.push(objetos[i].date)
-                 hora.push(objetos[i].time)
-                 precio.push(objetos[i].cost)
+        for (let i = 0; i < objetos.length; i++) {
+            if (objetos[i].UserId == idDelUsuarioLogueado/* MyUserId */) {
+                nombre.push(objetos[i].title)
+                stock.push(objetos[i].stock)
+                ciudad.push(objetos[i].city)
+                fecha.push(objetos[i].date)
+                hora.push(objetos[i].time)
+                precio.push(objetos[i].cost)
 
-             }
+            }
 
         }
 
         /* console.log(nombre, "soy el titulo del creado")
         console.log(stock, "soy el stock del creado") */
 
-        return(
+        return (
             <div>
                 <div>
-                <Container bg="white" mt={5} mb={5}>
-                <h2>Eventos que has creado</h2>
-                <Table striped hover>
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Nombre del evento</th>
-                                        <th>Ciudad</th>
-                                        <th>Fecha</th>
-                                        <th>Hora</th>
-                                        <th>Cantidad de entradas</th>
-                                        <th>Precio</th>
-                                        <th>Necesitas cambiar algo?</th>
+                    <Container bg="white" mt={5} mb={5}>
+                        <h2>Eventos que has creado</h2>
+                        <Table striped hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nombre del evento</th>
+                                    <th>Ciudad</th>
+                                    <th>Fecha</th>
+                                    <th>Hora</th>
+                                    <th>Cantidad de entradas</th>
+                                    <th>Precio</th>
+                                    <th>Necesitas cambiar algo?</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {nombre?.map((e, index) => (
+                                    <tr key={index}>
+                                        <td>{index}</td>
+                                        <td>{e}</td>
+                                        <td>{ciudad[index]}</td>
+                                        <td>{fecha[index]}</td>
+                                        <td>{hora[index]}</td>
+                                        <td>{stock[index]}</td>
+                                        <td>$ {precio[index]}</td>
+                                        <td><button>modificar</button></td>
+
+
 
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    {nombre?.map((e, index) => (
-                                        <tr key={index}>
-                                            <td>{index}</td>
-                                            <td>{e}</td>
-                                            <td>{ciudad[index]}</td>
-                                            <td>{fecha[index]}</td>
-                                            <td>{hora[index]}</td>
-                                            <td>{stock[index]}</td>
-                                            <td>$ {precio[index]}</td>
-                                            <td><button>modificar</button></td>
-
-                                            
-
-                                        </tr>
-                                    ))}
+                                ))}
 
 
-                                </tbody>
-                            </Table>
-                            </Container>
+                            </tbody>
+                        </Table>
+                    </Container>
                 </div>
-                
+
             </div>
 
-            
+
         )
 
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 
-    
+
 }
 
 export default EventosCreadosPorElUsuario;
--- src/components/OrderDetail.jsx
+++ src/components/OrderDetail.jsx
@@ -17,59 +17,78 @@ import { User } from "@auth0/auth0-react";
 
 export default function OrderDetail() {
   const allOrders = useSelector((state) => state.allOrders);
-  console.log(allOrders, "orders");
+  console.log(allOrders, "soy las ordenes");
+
   const dispatch = useDispatch();
-  const [idTicket, setIdTicket] = useState();
+  /* const [idTicket, setIdTicket] = useState(); */
+
   useEffect(() => {
     dispatch(getAllOrders());
   }, [dispatch]);
 
- 
- try {
-  const obj = allOrders?.orders?.map((e) => e.User);
-  
-  console.log(arregloGeneral, "obj1");
 
-  const ticketsito = allOrders?.orders?.map((e) => e.Tickets);
-  
-  console.log(ticketsito, "tickets")
-   let ids; 
-  for (let j = 0; j < ticketsito.length; j++) {
-    ids = ticketsito[j].map((e) => e);
+  try {
+    const usuario = allOrders?.orders?.map((e) => e.User);
+    console.log(usuario, " soy el usuario que creo la orden"); //esto queda (son los datos del usuario que creo la orden)
 
+
+    const ticketsito = allOrders?.orders?.map((e) => e.Tickets);
+    console.log(ticketsito, "soy el tickets de la orden")
+
+    /* for (let p = 0; p < ticketsito.length; p++){
+      console.log(ticketsito[p].id)
+
+
+    } */
+
+    /* let ticketsConJoin = ticketsito.join(',')
+    console.log(ticketsConJoin, "soy los tickets con el join") */
+
+    //--------------------------------------------------------------------------------------------------------------------------
+
+    let soloIdsDeTickets = [];/*  = ticketsito[0].map(e => e.id)
+    console.log(soloIdsDeTickets, "quiero solo los ids de los tickets") */ // hasta aca tengo un ticket en el arreglo (en formad e string)
     
+    for (let y = 0; y<ticketsito.length; y++){
+      soloIdsDeTickets.push(ticketsito[y].map(e => e.id))
+    }
+    console.log(soloIdsDeTickets, "quiero solo los ids de los tickets")
 
-    console.log(ids, "resultado");
-    var arregloGeneral= [...obj, ...ids]
-  }
+    let ticketsJoin = soloIdsDeTickets.join(",")
+    console.log(ticketsJoin, "soy join")
 
-  console.log(arregloGeneral, "generalTodo")
- } catch (error) {
-   console.log(error)
-   
- }
+    let ticketSlpit = ticketsJoin.split(",")
+    console.log(ticketSlpit, "soy slpit")
 
-  // 
+    //-------------------------------------------------------------------------------------------------------------------------
 
-  
-  
+    // aca voy a intentar tomar el nombre y el apellido del usuario
+    let nombre = []
+    let apellido = []
+    let idDelUsuario = []
+    let precioTotal = []
+    for (let t = 0; t < usuario.length; t++) {
+      nombre.push(usuario[t].name)
+      apellido.push(usuario[t].lastName)
+      idDelUsuario.push(usuario[t].id)
+    }
 
-  //   return (
-  //   <div>
+    let nombreSolo = [...new Set(nombre)]
+    let apellidoSolo = [...new Set(apellido)]
 
-  //   </div>
-  //   );
+    console.log(nombreSolo, "soy el nombre del usuario")
+    console.log(apellidoSolo, "soy el apellido del usuario")
 
-  // };
+    //--------------------------------------------------------------------------------------------------------------------
 
-  // export default OrderDetail;
 
-  return (
-    <div className={styles.Container}>
-      <NavTop />
 
+    return (
       <div className={styles.Container}>
-        
+        <NavTop />
+
+        <div className={styles.Container}>
+
           <Container fluid>
             <Row>
               <Col className={styles.container}>
@@ -79,111 +98,49 @@ export default function OrderDetail() {
                   <table className="table  text-center">
                     <thead>
                       <tr>
-                        <th scope="col"> </th>
-                        <th scope="col"></th>
+                        <th scope="col">Index </th>
                         <th scope="col">Ticket ID </th>
+                        <th scope="col">Nombre del evento</th>
                         <th scope="col">Id de Usuario </th>
-                        <th scope="col">Fecha </th>
-                        <th scope="col">Cantidad </th>
-                        <th scope="col">Precio Total </th>
-                        <th scope="col">Estado </th>
                         <th scope="col">Nombre </th>
                         <th scope="col">Apellido </th>
-                     
-                       
-                        
                       </tr>
                     </thead>
-                    {arregloGeneral ? arregloGeneral?.map( e =>  (
-                    <tbody>
-                      <tr>
-                        <th scope="row"></th>
-                        <th scope="row">
-                          <img style={{ width: "4rem" }} />
-                        </th>
-                        {/* <td>{idTicke()}</td>
-                        {/* <td>{allOrders.orders?.map((e) => e.UserId)} </td> */}
-                         {/* <td> {allOrders.orders?.map((e) => e.date)}</td>
-                        <td>
-                          {" "}
-                          {allOrders.orders?.map((e) => e.quantity + " ")}
-                        </td>
-                        <td>
-                          {allOrders.orders?.map((e) => e.totalPrice + " ")}
-                        </td>
-                        <td>{allOrders.orders?.map((e) => e.status + " ")}</td>
-                        <td> {allOrders.orders[1]?.User?.name}</td>
-                        <td> {allOrders.orders[1]?.User?.lastName}</td> */}
-                        
-                        <td> {e.externalId}</td>  
-                        <td> {e.name}</td>
-                        <td> {e.EventId}</td>
-                        <td> {e.lastName}</td>
-                        <td>{e.email} </td>
-                       
-                        <td> {e.totalPrice}</td>
+                    {ticketSlpit?.map((e, index) => (
+                      <tr key={index}>
+                        {<td>{index}</td>}
+                        <td>{ticketSlpit[index]}</td>
+                        <td>{ }</td>
+                        <td>{idDelUsuario}</td>
+                        <td>{nombreSolo}</td>
+                        <td>{apellidoSolo}</td>
+
+
+
+
                       </tr>
-                    </tbody>
-                      )) : (
-                        <Alert variant="success">
-                          <Alert.Heading>No hay ordenes de Tickets</Alert.Heading>
-              
-                          <p className="mb-0">Parece que no hay ninguna orden de Tickets</p>
-                        </Alert>
-                      )}
+                    ))}
                   </table>
                 </div>
               </Col>
             </Row>
           </Container>
+        </div>
+        <Container fluid>
+          <Row>
+            <Col style={{ marginTop: "370px" }}> </Col>
+          </Row>
+        </Container>
       </div>
-      <Container fluid>
-        <Row>
-          <Col style={{ marginTop: "370px" }}> </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+    );
+
+
+  } catch (error) {
+    console.log(error)
+
+  }
+
 }
 
 
 
-
-
-
-const order = {
-  msg: "Orden Creada",
-  orderRes: {
-    id: 6,
-    status: "Pending",
-    date: "Tue May 10 2022 14:40:51 GMT-0300 (hora estándar de Argentina)",
-    quantity: 1,
-    totalPrice: 1500,
-    createdAt: "2022-05-10T17:40:51.305Z",
-    updatedAt: "2022-05-10T17:40:51.305Z",
-    UserId: "b23e456f-c0d7-449b-ba74-6ab4279ae7df",
-    Tickets: [
-      {
-        id: "8a60918b-2505-4d20-a13c-6e10a1e0aefc",
-        status: "Reservado",
-        createdAt: "2022-05-10T17:37:23.226Z",
-        updatedAt: "2022-05-10T17:40:51.328Z",
-        OrderId: 6,
-        EventId: "a4bd45ee-658a-40c9-9a37-a5ab4c7cf70e",
-      },
-    ],
-    User: {
-      id: "b23e456f-c0d7-449b-ba74-6ab4279ae7df",
-      externalId: "google-oauth2|100691098426977273143",
-      name: "La sara",
-      lastName: "sara",
-      email: "blackwilson1495@gmail.com",
-      roll: "User",
-      picture: "jajajajajjaj",
-      city: null,
-      state: false,
-      createdAt: "2022-05-10T17:37:52.536Z",
-      updatedAt: "2022-05-10T17:37:52.536Z",
-    },
-  },
-};
