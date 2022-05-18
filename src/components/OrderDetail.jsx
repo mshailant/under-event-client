5eb6ec74dd4d5f99848f6a75a509834c82ab0d92
sebitacasa <seba.-14@hotmail.com>
2022-05-17 23:44:58 -0300

    holi

--- src/components/OrderDetail.jsx
+++ src/components/OrderDetail.jsx
@@ -14,123 +14,115 @@ import { Link } from "react-router-dom";
 import Carousely from "./Carousel";
 import { ajaxUpload } from "rsuite/esm/utils";
 import { User } from "@auth0/auth0-react";
+import CartItemOrder from "./DetailOrdenCart";
 
 export default function OrderDetail() {
   const allOrders = useSelector((state) => state.allOrders);
-  console.log(allOrders, 'ordenes')
-  const events = useSelector((state) => state.eventosBack)
- console.log(events)
+  console.log(allOrders, "ordenes");
+  const events = useSelector((state) => state.eventosBack);
+  console.log(events);
 
   const dispatch = useDispatch();
   const [idTicket, setIdTicket] = useState();
   useEffect(() => {
     dispatch(getAllOrders());
-    dispatch(getAllEvent())
+    dispatch(getAllEvent());
   }, [dispatch]);
 
- 
- try {
-  var obj = allOrders.orders?.map((e) => e.User);
-  console.log(obj, "soy el obj") // un arreglo de un objeto de info de usuario // 
-  // var names = allOrders.orderes?.map((e) => e.name)
-  // var lastName = allOrders.orderes?.map((e) => e.lastName)
+  try {
+    var obj = allOrders.orders?.map((e) => e.User);
+    console.log(obj, "soy el obj"); // un arreglo de un objeto de info de usuario //
+    // var names = allOrders.orderes?.map((e) => e.name)
+    // var lastName = allOrders.orderes?.map((e) => e.lastName)
 
-  var setNames = [...new Set(allOrders.orders.map(e => e.User.name))]
- 
-   var setLastNames =  [...new Set(allOrders.orders.map(e => e.User.lastName))]
-  var setObj = [...new Set(obj.map(e => e.email))]
-  var setPrice = [...new Set(allOrders.orders.map(e => e.totalPrice))]
-  
- 
-  
-  
+    var setNames = [...new Set(allOrders.orders.map((e) => e.User.name))];
 
-  const ticketsito = allOrders?.orders?.map((e) => e.Tickets);
-  
- 
-  var data = []
-  
-   let ids; 
-  for (let j = 0; j < ticketsito.length; j++) {
-    ids = ticketsito[j]?.map((e) => e.EventId);
-    
-    data.push(ids)
+    var setLastNames = [
+      ...new Set(allOrders.orders.map((e) => e.User.lastName)),
+    ];
+    var setObj = [...new Set(obj.map((e) => e.email))];
+    var setPrice = [...new Set(allOrders.orders.map((e) => e.totalPrice))];
 
-   
-    console.log(data, "EventIdssssssss")
-  }
-    
+    const ticketsito = allOrders?.orders?.map((e) => e.Tickets);
+    console.log(ticketsito, "tickets");
 
-//tengo los ids de los eventos pero ahora quiero el nombre
-let nombres = []
-let fechas = []
+    var dataTickets = [];
 
+    var tickets;
+    for (let f = 0; f < ticketsito.length; f++) {
+      tickets = ticketsito[f]?.map((e) => e.id);
+      dataTickets.push(tickets.join(" / ").split(" "));
 
-for (let n = 0; n < events.length; n++) {
-    for (let p = 0; p < data.length; p++) {
-        if (events[n].id === data[p]) {
-            nombres.push(events[n].title)
-            fechas.push(events[n].date)
-            
-        
-            
+      console.log(dataTickets, "idSSSS");
+    }
+
+    var data = [];
+
+    var ids;
+    for (let j = 0; j < ticketsito.length; j++) {
+      ids = ticketsito[j]?.map((e) => e.EventId);
+      data.push(ids.join(""));
+
+      console.log(data, "eventIDsss");
+    }
+
+    //tengo los ids de los eventos pero ahora quiero el nombre
+    var nombres = [];
+    var fechas = [];
+
+    for (let n = 0; n < events.length; n++) {
+      for (let p = 0; p < data.length; p++)
+        for (let i = 0; i < data[p].length; i++) {
+          if (events[n].id === data[i]) {
+            nombres.push(events[n].title);
+            fechas.push(events[n].date);
+            var setNombres = [...new Set(nombres.map((e) => e))];
+            var setFechas = [...new Set(fechas.map((e) => e))];
+          }
         }
     }
-}
 
+    var nombresNuevos = [setNombres.join(" ,").split(" ,")];
+    var fechasHoy = [setFechas.join(" ,").split(" ,")];
 
-var nombresNuevos = [nombres.join(' ,').split(' ,')]
-var fechasHoy = [fechas.join(' ,').split(' ,')]
-console.log(nombresNuevos, "nombres nuevos")
- var unicosElementos = []
-        var almacenadorDeVecesRepetidas = []
+    var unicosElementos = [];
+    var almacenadorDeVecesRepetidas = [];
 
-        var unicosElementosFechas = []
-        var  almacenadorDeVecesRepetidasFechas = []
+    var unicosElementosFechas = [];
+    var almacenadorDeVecesRepetidasFechas = [];
 
+    let contador = 1;
 
-        
+    for (let i = 0; i < nombresNuevos.length; i++) {
+      if (nombresNuevos[i + 1] === nombresNuevos[i]) {
+        //console.log(coleccionConSort[i], "soy la vez")
+        contador++;
+      } else {
+        unicosElementos.push(nombresNuevos[i]);
+        almacenadorDeVecesRepetidas.push(contador);
+        contador = 1;
+      }
+    }
 
-        let contador = 1
+    let contadores = 1;
 
-        for (let i = 0; i < nombresNuevos.length; i++) {
-            if (nombresNuevos[i + 1] === nombresNuevos[i]) {
-                //console.log(coleccionConSort[i], "soy la vez")
-                contador++
-            } else {
-                unicosElementos.push(nombresNuevos[i])
-                almacenadorDeVecesRepetidas.push(contador)
-                contador = 1
-                console.log(unicosElementos, "nombres")
-            }
-        }
+    for (let i = 0; i < fechasHoy.length; i++) {
+      if (fechasHoy[i + 1] === fechasHoy[i]) {
+        // console.log(coleccionConSort[i], "soy la vez")
+        contadores++;
+      } else {
+        unicosElementosFechas.push(fechasHoy[i]);
+        almacenadorDeVecesRepetidasFechas.push(contadores);
+        contadores = 1;
+      }
+    }
+    // console.log(unicosElementosFechas, "soy unicos elementos fechas")
+    // console.log(almacenadorDeVecesRepetidas, "soy almacenador de veces repetidas")
+  } catch (error) {
+    console.log(error);
+  }
 
-        
-        let contadores = 1
-
-        for (let i = 0; i <  fechasHoy.length; i++) {
-            if ( fechasHoy[i + 1] ===  fechasHoy[i]) {
-                // console.log(coleccionConSort[i], "soy la vez")
-                contadores++
-            } else {
-                unicosElementosFechas.push(fechasHoy[i])
-                almacenadorDeVecesRepetidasFechas.push(contadores)
-                contadores = 1
-            }
-        }
-        console.log(unicosElementosFechas, "soy unicos elementos fechas")
-        console.log(almacenadorDeVecesRepetidas, "soy almacenador de veces repetidas") 
-
-  
- } catch (error) {
-   console.log(error)
-   
- }
-
-  // 
-
-  
-  
+  //
 
   //   return (
   //   <div>
@@ -147,37 +139,32 @@ console.log(nombresNuevos, "nombres nuevos")
       <NavTop />
 
       <div className={styles.Container}>
-        
-          <Container fluid>
-            <Row>
-              <Col className={styles.container}>
-                <div className="row justify-content-center"></div>
+        <Container fluid>
+          <Row>
+            <Col className={styles.container}>
+              <div className="row justify-content-center"></div>
 
-                <div className="row mt-3">
-                  <table className="table  text-center">
-                    <thead>
-                      <tr>
-                        <th scope="col"> </th>
-                        <th scope="col"></th>
-                        <th scope="col">Email </th>
-                        
-                        
-                        <th scope="col">Fecha </th>
-                        <th scope="col">Precio Total </th>
-                        <th scope="col">Cantidad </th>
-                        <th scope="col">Nombre </th>
-                        <th scope="col">Apellido </th>
-                        <th scope="col">Estado </th>
-                        <th scope="col">Ticket ID </th>
-                        <th scope="col">Id de evento </th>
-                        <th scope="col">Id de evento </th>
-                     
-                       
-                        
-                      </tr>
-                    </thead>
-                    { allOrders ? (
-                    <tbody>
+              <div className="row mt-3">
+                <table className="table  text-center">
+                  <thead>
+                    <tr>
+                      <th scope="col"> </th>
+                      <th scope="col"></th>
+                      <th scope="col">Email </th>
+
+                      <th scope="col">Fecha </th>
+                      <th scope="col">Ticket ID </th>
+                      <th scope="col">Precio Total</th>
+                      <th scope="col">Cantidad</th>
+                      <th scope="col">Nombre </th>
+                      <th scope="col">Apellido </th>
+                      <th scope="col">Ticket ID </th>
+                      <th scope="col">Banda </th>
+                      <th scope="col">Cantidad  </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {allOrders ? (
                       <tr>
                         <th scope="row"></th>
                         <th scope="row">
@@ -185,7 +172,7 @@ console.log(nombresNuevos, "nombres nuevos")
                         </th>
                         {/* <td>{idTicke()}</td>
                         {/* <td>{allOrders.orders?.map((e) => e.UserId)} </td> */}
-                         {/* <td> {allOrders.orders?.map((e) => e.date)}</td>
+                        {/* <td> {allOrders.orders?.map((e) => e.date)}</td>
                         <td>
                           {" "}
                           {allOrders.orders?.map((e) => e.quantity + " ")}
@@ -196,30 +183,29 @@ console.log(nombresNuevos, "nombres nuevos")
                         <td>{allOrders.orders?.map((e) => e.status + " ")}</td>
                         <td> {allOrders.orders[1]?.User?.name}</td>
                         <td> {allOrders.orders[1]?.User?.lastName}</td> */}
-
-                      
-                        
-                         <td> {setObj}</td> 
-                         <td>{unicosElementosFechas} </td> 
-                         
-                         <td>  {setPrice}.00</td> 
-                         <td> {allOrders.orders?.map(e => e.quantity)}</td>
-                         <td> {setNames}</td>  
-                         <td> {setLastNames}</td> 
-                         <td></td> 
-                         <td> {unicosElementos}</td> 
-                         <td> {almacenadorDeVecesRepetidas}</td>
                        
+
+                         <td> {setObj}</td>
+                        <td>{unicosElementosFechas} </td>
+                        <td>{dataTickets.map(e => e)}</td>
+
+                        <td> {setPrice}.00</td>
+                        <td> {allOrders.orders?.map((e) => e.quantity)}</td>
+                        <td> {setNames}</td>
+                        <td> {setLastNames}</td>
+                        <td></td>
+                        <td> {unicosElementos}</td>
+                        <td> {almacenadorDeVecesRepetidas}</td> 
                       </tr>
+                  ) : (
+                    <h1>error</h1>
+                    )}
                     </tbody>
-                      
-                        
-                      ):<h1>error</h1>}
-                  </table>
-                </div>
-              </Col>
-            </Row>
-          </Container>
+                </table>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
       <Container fluid>
         <Row>
@@ -229,11 +215,6 @@ console.log(nombresNuevos, "nombres nuevos")
     </div>
   );
 }
-
-
-
-
-
 
 const order = {
   msg: "Orden Creada",
