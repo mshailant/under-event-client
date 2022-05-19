fa0a0ef2cc93185ae27911b4d3a97c6814285965
msh <mshailant@gmail.com>
2022-05-19 06:32:50 -0300

    update evento

--- src/components/ClientEvent.jsx
+++ src/components/ClientEvent.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { putEventClient } from "../redux/actions/actions";
 import { Button, Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import styles from './ClientEvent.module.css'
+import styles from "./ClientEvent.module.css";
 
 export default function ClientEvent({ openModal, setOpenModal, e }) {
   const [eventData, setEventData] = useState({
@@ -14,6 +14,16 @@ export default function ClientEvent({ openModal, setOpenModal, e }) {
     cost: "",
   });
 
+  useEffect(() => {
+    setEventData({
+      title: e?.title,
+      city: e?.city,
+      date: e?.date,
+      stock: e?.stock,
+      cost: e?.cost,
+    });
+  }, []);
+
   const dispatch = useDispatch();
   function handleSubmit(event) {
     event.preventDefault();
@@ -22,25 +32,22 @@ export default function ClientEvent({ openModal, setOpenModal, e }) {
       eventData.city ||
       eventData.date ||
       eventData.stock ||
-      eventData.cost 
-    
-    )  {
-       
-    dispatch(putEventClient(e.id, eventData));
+      eventData.cost
+    ) {
+      console.log("eee", e.UserId);
+      dispatch(putEventClient(e.id, eventData));
       alert("se modificaron los datos");
-      setOpenModal(false)
+      setOpenModal(false);
       setEventData({
         title: "",
         city: "",
         date: "",
         stock: "",
         cost: "",
-      })
+      });
     } else {
       alert("debe modificar algun dato");
-      
     }
-
   }
 
   function handleClose() {
@@ -52,8 +59,9 @@ export default function ClientEvent({ openModal, setOpenModal, e }) {
       ...eventData,
       [event.target.name]: event.target.value,
     });
-    
   };
+
+  console.log("asd", e);
 
   return (
     <Modal show={openModal} onHide={handleClose}>
@@ -62,19 +70,49 @@ export default function ClientEvent({ openModal, setOpenModal, e }) {
       </Modal.Header>
 
       <Modal.Body>
-          <div className={styles.containerModal}>
-        <label>nombre del evento</label>
-        <input onChange={handleChange} name="title" value={eventData.title} placeholder={e?.title} />
-        <label>ciudad</label>
-        <input onChange={handleChange} name="city" value={eventData.city} placeholder={e?.city} />
-        <label>fecha</label>
-        <input onChange={handleChange} name="date" value={eventData.date} placeholder={e?.date} />
-        <label>hora del evento</label>
-        <input onChange={handleChange} name="time" value={eventData.time} placeholder={e?.time} />
-        <label>cantidad</label>
-        <input onChange={handleChange} name="stock" value={eventData.stock} placeholder={e?.stock} />
-        <label>costo </label>
-        <input onChange={handleChange} name="cost" value={eventData.cost} placeholder={e?.cost} />
+        <div className={styles.containerModal}>
+          <label>nombre del evento</label>
+          <input
+            onChange={handleChange}
+            name="title"
+            value={eventData.title}
+            placeholder={e?.title}
+          />
+          <label>ciudad</label>
+          <input
+            onChange={handleChange}
+            name="city"
+            value={eventData.city}
+            placeholder={e?.city}
+          />
+          <label>fecha</label>
+          <input
+            onChange={handleChange}
+            name="date"
+            value={eventData.date}
+            placeholder={e?.date}
+          />
+          <label>hora del evento</label>
+          <input
+            onChange={handleChange}
+            name="time"
+            value={eventData.time}
+            placeholder={e?.time}
+          />
+          <label>cantidad</label>
+          <input
+            onChange={handleChange}
+            name="stock"
+            value={eventData.stock}
+            placeholder={e?.stock}
+          />
+          <label>costo </label>
+          <input
+            onChange={handleChange}
+            name="cost"
+            value={eventData.cost}
+            placeholder={e?.cost}
+          />
         </div>
       </Modal.Body>
       <Modal.Footer>
